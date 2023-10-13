dd79ccd0b87f2d3736ef900937b06e8d924e853b
Dom <szborzdynski@gmail.com>
2023-10-13 23:43:32 +0200

    działa poprawnie, ale brakuje optymalizacji

--- main.cpp
+++ main.cpp
@@ -5,12 +5,29 @@
 
 using namespace std;
 
-void permutacja(vector<int>& array){
+void permutacja(vector<int>& array, int &sciezka,vector<int>& najkrotsza, vector<vector<int> > macierz){
+    int obliczona = 0;
     do {
         for(int i : array){
             cout<<i<<" ";
         }
         cout<<endl;
+
+        obliczona = obliczona + macierz[0][array.front()];    // sciezka z startu do pierwszego miasta
+
+        for(int j = 0; j < array.size()-1; j++){    // w array są tylko miasta po drodze bez sciezek z start, do start
+            obliczona = obliczona + macierz[ array[j] ][ array[j+1] ];
+        }
+
+        obliczona = obliczona + macierz[0][array.back()];  // sciezka z ostatniego maista do startu
+
+        if (obliczona < sciezka){
+            sciezka = obliczona;
+            najkrotsza = array;
+        }
+
+        obliczona = 0;
+
         } while ( next_permutation( array.begin(),array.end() ) );
 }
 
@@ -59,23 +76,29 @@ int wczytaj_liczbe_miast(const string& daneWejsciowe){
 
 
 int main() {
+    int sciezka = INT_MAX;
     int liczba_miast;
     vector<int> tablica_miast;
+    vector<int> najkrotsza;
+
     cout<<"Podaj liczbe miast: ";
     cin>>liczba_miast;
-    cout<<endl<<endl;
+    cout<<endl;
 
     vector<vector<int> > macierz = wczytaj_macierz(to_string(liczba_miast) + "_test.txt");
     liczba_miast = wczytaj_liczbe_miast(to_string(liczba_miast) + "_test.txt");
 
-    for (int i=1; i<liczba_miast; ++i){
+    for (int i=1; i<liczba_miast; i++){
         tablica_miast.push_back(i);
     }
 
-    cout<<"liczba miast: "<<liczba_miast<<endl;
 
-    permutacja(tablica_miast);
-
+    permutacja(tablica_miast, sciezka, najkrotsza, macierz);
+    cout<<"najkrotsza sciezka dlugosc: "<<sciezka<<endl;
+    cout<<"najkrotsza sciezka przez miasta: ";
+    for (int j : najkrotsza){
+        cout<< j <<" ";
+    }
 
     return 0;
 }
