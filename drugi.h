afde5f1782b62849542b0b9881f86044682e2910
Dom <szborzdynski@gmail.com>
2023-10-20 17:51:50 +0200

    usuniento niepotrzbne rzeczy

--- drugi.h
+++ drugi.h
@@ -7,8 +7,6 @@ using namespace std;
 class drugi {
 public:
 
-    void Stop();
-
     static void
     Branch_and_bound(vector<int> &permutacja, int &sciezka, vector<int> &najkrotsza, vector<vector<int>>);
 };
