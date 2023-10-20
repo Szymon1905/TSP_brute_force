b5ab8bdefab7c823ce34427400cc521052c988b7
Dom <szborzdynski@gmail.com>
2023-10-20 18:12:43 +0200

    dodano komentarze

--- drugi.cpp
+++ drugi.cpp
@@ -15,17 +15,17 @@ void drugi::Branch_and_bound(vector<int>& permutacja, int &sciezka, vector<int>&
 
         obliczona = obliczona + macierz[0][permutacja.front()];    // sciezka ze startu do pierwszego miasta
 
-        if (obliczona > sciezka){
+        if (obliczona > sciezka){   // sprawdzmy czy scieżka sie opłaca na samym początku
             obliczona = 0;
-            int przesuw = permutacja.front();
+            int przesuw = permutacja.front();   // obliczam o ile permutacji należy się przesunąć gdyż nie będziemy ich sprawdzać
             while (przesuw == permutacja.front()){
                 czy_dalej = next_permutation(permutacja.begin(),permutacja.end());
                 if (!czy_dalej){
-                    return;
+                    return;  // jeśli nie istnieje to koniec
                 }
             }
 
-            goto skip;
+            goto skip;      // kontynujemy jeśli permutacja istnieje
         }
 
         for(int j = 0; j < permutacja.size() - 1; j++){
@@ -33,17 +33,17 @@ void drugi::Branch_and_bound(vector<int>& permutacja, int &sciezka, vector<int>&
             obliczona = obliczona + droga;  // po drodze bez sciezek ze start oraz do start
 
 
-            if (obliczona > sciezka){
+            if (obliczona > sciezka){   // sprawdzmy czy scieżka sie opłaca (kolejne po początku)
                 obliczona = 0;
-                int przesuw = permutacja[j+1];
+                int przesuw = permutacja[j+1];   // obliczam o ile permutacji należy się przesunąć gdyż nie będziemy ich sprawdzać
                 while (przesuw == permutacja[j+1]){
-                    czy_dalej = next_permutation(permutacja.begin(),permutacja.end());
+                    czy_dalej = next_permutation(permutacja.begin(),permutacja.end());  // sprawdzamy czy istnieje kolejna permutacja
                     if (!czy_dalej){
-                        return;
+                        return; // jeśli nie istnieje to koniec
                     }
                 }
 
-                goto skip;
+                goto skip;   // kontynujemy jeśli permutacja istnieje
             }
         }
 
